9d4240b13e8989eea14801c7d2fdc55d71943355
te260ku <teturou@aw.wakwak.com>
2021-10-22 18:50:41 +0900

    update twist publisher script

--- ROS2UnityBasicProject/Assets/Scripts/TwistPublisher.cs
+++ ROS2UnityBasicProject/Assets/Scripts/TwistPublisher.cs
@@ -7,11 +7,18 @@ public class TwistPublisher : MonoBehaviour
 {
     [SerializeField]
     private string topicName;
+    [SerializeField]
+    private MANIPULATION_MODE manipulationMode;
+    private enum MANIPULATION_MODE {
+        EXTERNAL, 
+        KEY
+    }
     private ROSConnection ros;
     private float timeElapsed;
     private float publishMessageFrequency = 0.5f;
     private float linearSpeed = 0.5f;
     private float angularSpeed = 0.2f;
+    private TwistMsg twist = new TwistMsg();
 
     void Start()
     {
@@ -26,34 +33,39 @@ public class TwistPublisher : MonoBehaviour
         // 一定周期で中の処理を実行する
         if (timeElapsed > publishMessageFrequency)
         {
-            // 移動と回転の速度を初期化
-            Vector3 linear = new Vector3(0f, 0f, 0f);
-            Vector3 angular = new Vector3(0f, 0f, 0f);
-  
-            // 押された矢印キーの方向によって速度を与える
-            if (Input.GetKey(KeyCode.UpArrow)) {
-                linear.z = linearSpeed;
-            }
-            if (Input.GetKey(KeyCode.DownArrow)) {
-                linear.z = -linearSpeed;
-            }
-            if (Input.GetKey(KeyCode.LeftArrow)) {
-                angular.y = -angularSpeed;
-            }
-            if (Input.GetKey(KeyCode.RightArrow)) {
-                angular.y = angularSpeed;
-            }
+            switch (manipulationMode)
+            {
+                case MANIPULATION_MODE.EXTERNAL:
+                    break;
 
-            // 移動速度と回転速度をそれぞれROSの座標系に変換
-            Vector3<FLU> rosLinear = linear.To<FLU>();
-            Vector3<FLU> rosAngular = angular.To<FLU>();
-            rosAngular.z *= -1f;
+                case MANIPULATION_MODE.KEY:
+                    // 移動と回転の速度を初期化
+                    Vector3 linear = new Vector3(0f, 0f, 0f);
+                    Vector3 angular = new Vector3(0f, 0f, 0f);
+        
+                    // 押された矢印キーの方向によって速度を与える
+                    if (Input.GetKey(KeyCode.UpArrow)) {
+                        linear.z = linearSpeed;
+                    }
+                    if (Input.GetKey(KeyCode.DownArrow)) {
+                        linear.z = -linearSpeed;
+                    }
+                    if (Input.GetKey(KeyCode.LeftArrow)) {
+                        angular.y = -angularSpeed;
+                    }
+                    if (Input.GetKey(KeyCode.RightArrow)) {
+                        angular.y = angularSpeed;
+                    }
 
-            Vector3Msg l = new Vector3Msg((float)linear.x, (float)linear.y, (float)linear.z);
-            Vector3Msg a = new Vector3Msg((float)angular.x, (float)angular.y, (float)angular.z);
+                    // 速度をTwistMsgインスタンスにセット
+                    SetTwistMsgValue(linear, angular);
 
-            // 移動と回転を組み合わせたTwistMsgを作成
-            TwistMsg twist = new TwistMsg(rosLinear, rosAngular);
+                    break;
+
+                default:
+                    break;
+            }
+            
             
             SendTwistMsg(twist);
             
@@ -64,5 +76,16 @@ public class TwistPublisher : MonoBehaviour
     private void SendTwistMsg(TwistMsg msg)
     {
         ros.Publish(topicName, msg);
+    }
+
+    public void SetTwistMsgValue(Vector3 linearVector, Vector3 angularVector) {
+        // 移動速度と回転速度をそれぞれROSの座標系に変換
+        Vector3<FLU> rosLinear = linearVector.To<FLU>();
+        Vector3<FLU> rosAngular = angularVector.To<FLU>();
+        rosAngular.z *= -1f;
+
+        // 速度をTwistMsgインスタンスにセット
+        twist.linear = rosLinear;
+        twist.angular = rosAngular;
     }
 }
